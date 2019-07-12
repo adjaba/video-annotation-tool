11555a20ffd6f54a1d7ab8e9d6268ae7d471e36e
Andrea Jaba <jabaandrea@ncsoft.com>
2019-07-12 16:07:46 +0900

    removed node_modules, added video.js player

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react';
-import './App.css';
-import UploadVideos from './UploadVideos'
+import React, { Component } from "react";
+import "./App.css";
+import UploadVideos from "./UploadVideos";
+import VideoPlayer from "./VideoPlayer";
+import "video.js/dist/video-js.css";
 
-import {
-  Header,
-  Form,
-  Button
-} from 'semantic-ui-react';
+import { Header, Form, Button } from "semantic-ui-react";
 
 // (function localFileVideoPlayer() {
 // 	'use strict'
@@ -39,21 +37,32 @@ import {
 
 var URL = window.URL || window.webkitURL;
 
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [
+    {
+      src: "/path/to/video.mp4",
+      type: "video/mp4"
+    }
+  ]
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       video: null
-    }
+    };
     // this.handleFilesSubmit = this.handleFilesSubmit.bind(this);
     this.playSelectedFile = this.playSelectedFile.bind(this);
   }
-  
+
   playSelectedFile(event) {
     console.log(event.target.files[0]);
     var file = event.target.files[0];
     // var type = file.type
-    var videoNode = document.querySelector('video')
+    var videoNode = document.querySelector("video");
     // var canPlay = videoNode.canPlayType(type)
     // if (canPlay === '') canPlay = 'no'
     // var message = 'Can play type "' + type + '": ' + canPlay
@@ -64,11 +73,13 @@ class App extends Component {
     //   return
     // }
 
-    var fileURL = URL.createObjectURL(file)
-    videoNode.src = fileURL
+    var fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
+    videoJsOptions["sources"][0]["src"] = fileURL;
+    videoJsOptions["sources"][0]["type"] = file.type;
   }
 
-  // async handleFilesSubmit(e) { 
+  // async handleFilesSubmit(e) {
   //   console.log(e);
   //   console.log(e.target);
 
@@ -92,6 +103,7 @@ class App extends Component {
   //   myVideo.play();
   //   // form.reset();
   // }
+
   render() {
     return (
       <div>
@@ -110,26 +122,17 @@ class App extends Component {
           />
           <Button type="submit">Upload</Button>
         </Form> */}
-        <input id='input' type="file" accept="video/*" onChange={(e) => {this.setState({video: e.target.value}); this.playSelectedFile(e);}}/>
-        <video id="video" controls autoPlay/>
-        <head>
-          <link href="https://vjs.zencdn.net/7.6.0/video-js.css" rel="stylesheet"/>
-          <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"/>
-        </head>
-
-        <body>
-        <video id='my-video' class='video-js' controls preload='auto' width='640' height='264'
-        poster='MY_VIDEO_POSTER.jpg' data-setup='{}'>
-          <source src='MY_VIDEO.mp4' type='video/mp4' />
-          <source src='MY_VIDEO.webm' type='video/webm' />
-          <p class='vjs-no-js'>
-            To view this video please enable JavaScript, and consider upgrading to a web browser that
-            <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video </a>
-          </p>
-        </video>
-
-        <script src='https://vjs.zencdn.net/7.6.0/video.js'></script>
-      </body>
+        <input
+          id="input"
+          type="file"
+          accept="video/*"
+          onChange={e => {
+            this.setState({ video: e.target.value });
+            this.playSelectedFile(e);
+          }}
+        />
+        <video id="video" controls autoPlay />
+        {this.state.video ? <VideoPlayer {...videoJsOptions} /> : null}
       </div>
     );
   }
