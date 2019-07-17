c97f339af72b95e69c731bdf831e08cb37b52d4a
Andrea Jaba <jabaandrea@ncsoft.com>
2019-07-17 15:04:32 +0900

    changing into videoPreview component (build: fail)

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,12 @@ import UploadVideos from "./UploadVideos";
 import VideoPlayer from "./VideoPlayer";
 import "video.js/dist/video-js.css";
 import videojs from "video.js";
+import Event from "./Event";
+import VideoPreview from "./VideoPreview";
+import { frameToSecs } from "./utils";
 
-import { Header, Form, Button } from "semantic-ui-react";
+import { Header, Form, Button, Icon, List } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 // (function localFileVideoPlayer() {
 // 	'use strict'
@@ -56,13 +60,19 @@ class App extends Component {
     super(props);
     this.state = {
       videoName: null,
+      videoSrc: null,
       json: null,
-      metadata: null
+      metadata: Object(),
+      segmentStart: null,
+      segmentEnd: null,
+      segmentIndex: 0
     };
     this.playSelectedFile = this.playSelectedFile.bind(this);
     this.jumpTo = this.jumpTo.bind(this);
     this.parseJSONInput = this.parseJSONInput.bind(this);
-    this.frameToSecs = this.frameToSecs.bind(this);
+    // this.frameToSecs = this.frameToSecs.bind(this);
+    this.playSection = this.playSection.bind(this);
+    this.videoPreviewChange = this.videoPreviewChange.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -70,10 +80,11 @@ class App extends Component {
       prevState["videoName"] !== this.state.videoName ||
       prevState["json"] !== this.state.json;
     if (videoNameJSONUpdated && this.state.videoName && this.state.json) {
-      console.log(Object.keys(this.state.json));
       if (Object.keys(this.state.json).indexOf(this.state.videoName) >= 0) {
         this.setState({
-          metadata: this.state.json[this.state.videoName]
+          metadata: this.state.json[this.state.videoName],
+          segmentStart: 0,
+          segmentEnd: videojs.getPlayer("videoJS").duration()
         });
       } else {
         alert("Upload a video with the correct filename.");
@@ -81,13 +92,12 @@ class App extends Component {
     }
   }
 
+  // on video upload
   playSelectedFile(event) {
     var file = event.target.files[0];
-    const players = ["videoJS", "videoJSStart", "videoJSEnd"].map(id =>
-      videojs.getPlayer(id)
-    );
+    const player = videojs.getPlayer("videoJS");
 
-    if (players[0].canPlayType(file.type) === "") {
+    if (player.canPlayType(file.type) === "") {
       alert(
         "Cannot play video with type " +
           file.type +
@@ -97,14 +107,22 @@ class App extends Component {
     }
 
     var fileURL = URL.createObjectURL(file);
-    players.forEach(function(player) {
-      player.src({
-        src: fileURL,
-        type: file.type
-      });
+    player.src({
+      src: fileURL,
+      type: file.type
+    });
+
+    const playerStart = videojs.getPlayer("videoJSStart");
+    playerStart.src({
+      src: fileURL,
+      type: file.type
     });
     this.setState({
-      videoName: file.name.substring(0, file.name.lastIndexOf("."))
+      videoName: file.name.substring(0, file.name.lastIndexOf(".")),
+      videoSrc: {
+        src: fileURL,
+        type: file.type
+      }
     });
   }
 
@@ -124,26 +142,47 @@ class App extends Component {
     reader.readAsText(event.target.files[0]);
   }
 
-  frameToSecs(frame, fps) {
-    return Math.floor(frame / fps) + parseInt(frame % fps) / fps;
+  // play section if end > start
+  playSection() {
+    var myPlayer = videojs.getPlayer("videoJS");
+    var startInput = parseInt(document.getElementById("start").value);
+    var endInput = parseInt(document.getElementById("end").value);
+    var start = frameToSecs(startInput, this.state.metadata["fps"]) || 0;
+    var end =
+      frameToSecs(endInput, this.state.metadata["fps"]) || myPlayer.duration();
+
+    if (end > start) {
+      myPlayer.currentTime(start);
+      myPlayer.on("timeupdate", function(e) {
+        if (myPlayer.currentTime() >= end) {
+          console.log("paused", myPlayer.currentTime(), end);
+          myPlayer.pause();
+          myPlayer.off("timeupdate");
+        }
+      });
+      myPlayer.play();
+    } else {
+      alert("end time should be bigger than start time");
+      return;
+    }
   }
 
   jumpTo() {
     var myPlayer = videojs.getPlayer("videoJS");
     var startInput = parseInt(document.getElementById("start").value);
     var endInput = parseInt(document.getElementById("end").value);
-    var start = this.frameToSecs(startInput, 29.97) || 0;
-    var end = this.frameToSecs(endInput, 29.97) || myPlayer.duration();
-    console.log(start, end);
-    myPlayer.currentTime(start);
+    var start = frameToSecs(startInput, this.state.metadata["fps"]) || 0;
+    var end =
+      frameToSecs(endInput, this.state.metadata["fps"]) || myPlayer.duration();
 
-    myPlayer.off("timeupdate");
+    myPlayer.currentTime(start);
 
     if (end > start) {
       myPlayer.on("timeupdate", function(e) {
         if (myPlayer.currentTime() >= end) {
           console.log("paused", myPlayer.currentTime(), end);
           myPlayer.pause();
+          myPlayer.off("timeupdate");
         }
       });
     }
@@ -151,11 +190,11 @@ class App extends Component {
     myPlayer.play();
 
     var screenStart = videojs.getPlayer("videoJSStart");
-    console.log("screenStart setting" + screenStart.currentSrc());
     screenStart.currentTime(start);
 
-    var screenEnd = videojs.getPlayer("videoJSEnd");
-    screenEnd.currentTime(end);
+    // var screenEnd = videojs.getPlayer("videoJSEnd");
+    // console.log('test videopreview, ' + screenEnd)
+    // screenEnd.currentTime(end);
   }
   // async handleFilesSubmit(e) {
   //   console.log(e);
@@ -181,21 +220,50 @@ class App extends Component {
   //   myVideo.play();
   //   // form.reset();
   // }
+  videoPreviewChange(e) {
+    this.setState({
+      segmentEnd: parseInt(e.target.value)
+    });
+  }
 
+  renderEvents() {
+    return "annotations" in this.state.metadata
+      ? this.state.metadata["annotations"].map((prop, i) => (
+          <Event
+            key={i}
+            {...prop}
+            index={i}
+            onClick={() =>
+              this.setState({
+                segmentStart: prop["segment"][0],
+                segmentEnd: prop["segment"][1],
+                segmentIndex: prop["segmentIndex"] + 1
+              })
+            }
+          />
+        ))
+      : null;
+  }
   render() {
     return (
       <div style={{ display: "flex", height: "100vh", flexDirection: "row" }}>
         <div
           style={{
             display: "flex",
-            flex: "1 1 0",
             flexDirection: "column",
-            maxWidth: 300,
+            padding: "1em 0.5em",
+            borderRight: "1px solid #ccc",
             height: "100%",
-            backgroundColor: "#fff"
+            flex: 1,
+            maxWidth: 300
           }}
         >
-          Annotation
+          <Header size="large" style={{ flex: "0 0 auto" }}>
+            Events
+          </Header>
+          <List divided selection style={{ flex: 1, overflowY: "auto" }}>
+            {this.renderEvents()}
+          </List>
         </div>
         <div
           style={{
@@ -265,7 +333,16 @@ class App extends Component {
               <input type="number" id="start" onChange={this.jumpTo}></input>
               <VideoPlayer id="videoJSStart" {...videoPreviewOptions} />
             </div>
-            <div
+            {/* <VideoPreview name="start" time={this.state.segmentStart} key={this.state.segmentIndex+"start"} onChange={this.videoPreviewChange} fps= {this.state.metadata['fps']} src = {this.state.videoSrc}/> */}
+            <VideoPreview
+              name="end"
+              time={this.state.segmentEnd}
+              key={this.state.segmentIndex + "end"}
+              onChange={this.videoPreviewChange}
+              fps={this.state.metadata["fps"]}
+              src={this.state.videoSrc}
+            />
+            {/* <div
               style={{
                 display: "flex",
                 flexDirection: "column",
@@ -273,9 +350,19 @@ class App extends Component {
                 padding: 5
               }}
             >
-              <input type="number" id="end" onChange={this.jumpTo}></input>
+              <input type="number" id="end" default={this.state.segmentEnd} disabled= {!this.state.segmentEnd} onChange={this.jumpTo}></input>
               <VideoPlayer id="videoJSEnd" {...videoPreviewOptions} />
-            </div>
+            </div> */}
+            <Button
+              content="Play section"
+              icon="play"
+              labelPosition="left"
+              onClick={this.playSection}
+            />
+            <Button icon labelPosition="right">
+              <Icon name="save" />
+              Save
+            </Button>
           </div>
         </div>
       </div>
