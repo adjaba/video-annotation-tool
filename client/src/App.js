e7d5d967a7a65ee0546b960256345ccca9089bda
Andrea Jaba <jabaandrea@ncsoft.com>
2019-07-25 17:56:23 +0900

    responsive ui flex parent v2

--- client/src/App.js
+++ client/src/App.js
@@ -422,7 +422,7 @@ class App extends Component {
               width: "100%",
               flexDirection: "column",
               alignItems: "center",
-              flex: 1
+              paddingBottom: "10px"
             }}
           >
             <input
