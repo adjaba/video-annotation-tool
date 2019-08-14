4dcf7416c91d2fbf197feb7193233bce06a3100c
Andrea Jaba <jabaandrea@ncsoft.com>
2019-08-14 17:10:28 +0900

    redirect to working mediainfo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import AnnotationApp from "./AnnotationApp";
 import AdminApp from "./AdminApp";
+// import FPSpage from "./FPSpage";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Fragment>
           <Route path="/admin" component={AdminApp} />
           <Route exact path="/" component={AnnotationApp} />
+          <Route path='/fps'render={() => {window.location.href="/html/index.html"}}/>
         </Fragment>
       </Router>
     );
